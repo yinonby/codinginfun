7ed8cd62c94b13b51d3866970d7317defc0663d8
Yinon <yinonby@Yinons-MacBook-Pro.local>
2023-10-30 10:26:36 +0100

    create sub-folders for chapters and lessons

--- src/ExerciseMap.ts
+++ src/ExerciseMap.ts
@@ -1,10 +1,10 @@
 
-import BptsVarsBasic1ExerciseInfo from "./exercises/bpts/vars-basic-1/ExerciseInfo";
-import BptsVarsBasic1ExerciseTest from "./exercises/bpts/vars-basic-1/ExerciseTest";
-import BptsVarsBasic2ExerciseInfo from "./exercises/bpts/vars-basic-2/ExerciseInfo";
-import BptsVarsBasic2ExerciseTest from "./exercises/bpts/vars-basic-2/ExerciseTest";
-import BptsVarsBasic3ExerciseInfo from "./exercises/bpts/vars-basic-3/ExerciseInfo";
-import BptsVarsBasic3ExerciseTest from "./exercises/bpts/vars-basic-3/ExerciseTest";
+import BptsVarsBasic1ExerciseInfo from "./exercises/bpts/vars/basics/vars-basic-1/ExerciseInfo";
+import BptsVarsBasic1ExerciseTest from "./exercises/bpts/vars/basics/vars-basic-1/ExerciseTest";
+import BptsVarsBasic2ExerciseInfo from "./exercises/bpts/vars/basics/vars-basic-2/ExerciseInfo";
+import BptsVarsBasic2ExerciseTest from "./exercises/bpts/vars/basics/vars-basic-2/ExerciseTest";
+import BptsVarsBasic3ExerciseInfo from "./exercises/bpts/vars/basics/vars-basic-3/ExerciseInfo";
+import BptsVarsBasic3ExerciseTest from "./exercises/bpts/vars/basics/vars-basic-3/ExerciseTest";
 import ExerciseInfoAdapter from "./ExerciseInfoAdapter";
 import ExerciseTestAdapter from "./ExerciseTestAdapter";
 
