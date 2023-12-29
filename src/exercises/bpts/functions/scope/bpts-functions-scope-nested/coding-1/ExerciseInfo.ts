65c53dd7b43bd231cd07b0639754a2d92b9f2d21
Yinon-mac <yinonby@yahoo.com>
2023-12-29 23:14:18 +0100

    fix '4 spaces' to '2 spaces'

--- src/exercises/bpts/functions/scope/bpts-functions-scope-nested/coding-1/ExerciseInfo.ts
+++ src/exercises/bpts/functions/scope/bpts-functions-scope-nested/coding-1/ExerciseInfo.ts
@@ -6,8 +6,8 @@ const md: string = `
 - Then, leave the next line empty.
 - Then, in the next line, declare a function named <<foo>>, followed by an opening bracket <<{>>.
 - Then, in the next line, declare a constant variable named <<myName>>, of type <<string>>, and initialize its value to <<"Bob">>. Don't forget a semicolon <<;>> at the end of the line.
-- Then, in the next line, begin with 4 spaces, then print <<"Inside foo: myName is:">> to the output (console). Don't forget a semicolon <<;>> at the end of the line.
-- Then, in the next line, begin with 4 spaces, then print <<myName>> to the output (console). Don't forget a semicolon <<;>> at the end of the line.
+- Then, in the next line, begin with 2 spaces, then print <<"Inside foo: myName is:">> to the output (console). Don't forget a semicolon <<;>> at the end of the line.
+- Then, in the next line, begin with 2 spaces, then print <<myName>> to the output (console). Don't forget a semicolon <<;>> at the end of the line.
 - Then, in the next line, close the function using a closing bracket <<}>>.
 - Then, leave the next line empty.
 - Then, in the next line, print <<"Outside foo: myName is:">> to the output (console). Don't forget a semicolon <<;>> at the end of the line.
--- src/exercises/bpts/functions/scope/bpts-functions-scope-outer/coding-1/ExerciseInfo.ts
+++ src/exercises/bpts/functions/scope/bpts-functions-scope-outer/coding-1/ExerciseInfo.ts
@@ -5,8 +5,8 @@ const md: string = `
 - In the first line, declare a constant variable named <<myName>>, of type <<string>>, and initialize its value to <<"John">>. Don't forget a semicolon <<;>> at the end of the line.
 - Then, leave the next line empty.
 - Then, in the next line, declare a function named <<foo>>, followed by an opening bracket <<{>>.
-- Then, in the next line, begin with 4 spaces, then print <<"Inside foo: myName is:">> to the output (console). Don't forget a semicolon <<;>> at the end of the line.
-- Then, in the next line, begin with 4 spaces, then print <<myName>> to the output (console). Don't forget a semicolon <<;>> at the end of the line.
+- Then, in the next line, begin with 2 spaces, then print <<"Inside foo: myName is:">> to the output (console). Don't forget a semicolon <<;>> at the end of the line.
+- Then, in the next line, begin with 2 spaces, then print <<myName>> to the output (console). Don't forget a semicolon <<;>> at the end of the line.
 - Then, in the next line, close the function using a closing bracket <<}>>.
 - Then, leave the next line empty.
 - Then, in the next line, print <<"Outside foo: myName is:">> to the output (console). Don't forget a semicolon <<;>> at the end of the line.
